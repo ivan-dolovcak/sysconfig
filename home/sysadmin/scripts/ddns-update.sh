e9762b36f23b81ad7166c4e6e07209cc58619efb
Ivan Dolovčak <140179231+ivan-dolovcak@users.noreply.github.com>
2026-03-16 23:14:46 +0100

    script: update DDNS domain

--- home/sysadmin/scripts/ddns-update.sh
+++ home/sysadmin/scripts/ddns-update.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
+set -eu
 
-# https://www.duckdns.org/
-# This script sends my dynamic public IP to the DDNS service.
-
-# This script is in crontab (every 5 minutes).
-
-domain="stroberi"
-token="e1a9167b-88ce-4af4-a4ba-493b0cb6a1cf"
-logfile=".ddns-update.log"
+domain="dolovcak"
+token="$(cat /home/sysadmin/scripts/duckdns-token)"
+logfile="/var/log/ddns-update.log"
 url="https://www.duckdns.org/update?domains=$domain&token=$token&ip="
 
-curl --interface eth0 -k "$url" -o "$logfile" 
+curl "$url" --output "$logfile" 
